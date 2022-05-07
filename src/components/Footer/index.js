b818c9a9ea068d01c8aa8f114cf51400b5c8b362
Anthony Barragan <anthony.bar.89@gmail.com>
2022-05-06 23:52:51 -0700

    fixed background sizing to fit all screen sizes and stuck footer to the bottom of the page

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faGithub, faLinkedin, faStackOverflow} from '@fortawesome/free-brands-svg-icons'
 
@@ -7,9 +6,9 @@ import {faGithub, faLinkedin, faStackOverflow} from '@fortawesome/free-brands-sv
 
 function Footer() {
     return (
-        <footer className='d-flex justify-content-center'>
+        <footer>
             <a href='https://github.com/Abarragan89' target='_blank'><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon></a>
-            <a href='https://www.linkedin.com/in/anthony-barragan-ba4a12226/' target='_blank'className='px-5'><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
+            <a href='https://www.linkedin.com/in/anthony-barragan-ba4a12226/' target='_blank'><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></a>
             <a href='https://stackoverflow.com/users/17237403/anthony-barragan' target='_blank'><FontAwesomeIcon icon={faStackOverflow}></FontAwesomeIcon></a>
         </footer>
     )
