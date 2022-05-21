687c570e0263078827e469bde98a913f1f246d83
Anthony Barragan <anthony.bar.89@gmail.com>
2022-05-21 08:26:35 -0700

    fixed: add more possible home routes for home-footer rendering

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import {faGithub, faLinkedin, faStackOverflow} from '@fortawesome/free-brands-sv
 
 
 function Footer({ location }) {
-    const isHome = location.pathname === '/' || location.pathname === '/react-portfolio';
+    const isHome = location.pathname === '/' || location.pathname === '/react-portfolio' || location.pathname === '/react-portfolio/';
     console.log(isHome);
     console.log(location.pathname)
     return (
