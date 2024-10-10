2a923a62a7e92734a7a895a593289f269695d227
Anthony Barragan <anthony.bar.89@gmail.com>
2024-10-09 23:42:31 -0700

    removed require from img src in about me

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -12,7 +12,7 @@ function About () {
                         <p>"True Wisdom comes to each of us when we realize how little we understand about life, ourselves, and the world around us"</p>
                         <p>- Socrates</p>
                     </div>
-                    <img src={require('../../assets/small/profile-pic.JPG')} id='profile-pic' alt='headshot of Anthony Barragan'></img>
+                    <img src={'images/small/profile-pic.JPG'} id='profile-pic' alt='headshot of Anthony Barragan'></img>
                 </div>
             </div>
         </section>
