5c60de509ce3a1e0cf32c7eae9c9e60c210d213e
Anthony Barragan <anthony.bar.89@gmail.com>
2024-10-09 23:36:46 -0700

    redirect base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/abarragan89.github.io/'
+  plugins: [react()],
+  base: './'
 })
