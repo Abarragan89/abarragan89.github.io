93898db06f5109a0ad7191d7b708f942d1a6ac3d
Anthony Barragan <anthony.bar.89@gmail.com>
2024-10-16 08:12:30 -0700

    changed modal styles in buttons and clicking on the backdrop will close the modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,16 +1,18 @@
+import { IoCloseOutline } from "react-icons/io5";
 import './index.css';
 
 function Modal({ project, onClose }) {
+
     return (
-        <div className='modalBackdrop'>
-            <div className="modalContainer">
+        <div className='modalBackdrop' onClick={onClose}>
+            <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
+                <IoCloseOutline className='closeModalX' size='30px' onClick={onClose} />
                 <h3 className='modal-title'>{project.name}</h3>
                 <img className='modal-picture' src={`images/large/${project.photoRoute}.png`} alt='snapshot of chosen project' />
                 <p className='modal-description'>{project.description}</p>
                 <div id="modal-btn-group">
                     <a href={project.url} className='modal-links' target='_blank' rel='noopener noreferrer'>Website</a>
                     <a href={project.github} className='modal-links' target='_blank' rel='noopener noreferrer'>Github</a>
-                    <button type='button' className='modal-btn-close' onClick={onClose}>Close</button>
                 </div>
             </div>
         </div>
